5a450591b5a33247edc361a02a35ad42f11e965f
Youyan WU <wuyouyan1@gmail.com>
2019-12-17 19:46:32 +0100

    add some original tests

---
+++ src/Test.java
@@ -0,0 +1,29 @@
+public class Test {
+    public static void main(String[] args) {
+        /*TEST of Java！！！*/
+        /*int a = 5;
+        int b = 2;
+        System.out.println(a/b); //2*/
+        /*int i = 0;
+        System.out.println(i++); //0*/
+        /*int i1 = Integer.valueOf("1001",2);
+        int i2 = Integer.valueOf("1101",2);
+        String s = Integer.toBinaryString((i1&i2));//
+        System.out.println(s); //1001*/
+        /*A = 0011 1100
+        B = 0000 1101
+        -----------------
+        A&B = 0000 1100
+        ~A= 1100 0011
+        A | B = 0011 1101
+        A ^ B = 0011 0001*/
+       /* System.out.println( Planet.MERCURY.equals(Planet.MERCURY) );//true
+        System.out.println( Planet.MERCURY==Planet.MERCURY );//true
+        System.out.println( Planet.MERCURY==Planet.VENUS );//false*
+
+    }
+        /*enum Planet {
+            MERCURY, VENUS, EARTH
+        }*/
+    }
+}
