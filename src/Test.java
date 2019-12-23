f8fddf18e858da41c5a2b46062e66e61372618b3
Youyan WU <wuyouyan1@gmail.com>
2019-12-23 14:56:10 +0100

    line separator 换行符

--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         /*TEST of Java！！！*/
-        /*int a = 5;
-        int b = 2;
-        System.out.println(a/b); //2*/
-        /*int i = 0;
-        System.out.println(i++); //0*/
-        /*int i1 = Integer.valueOf("1001",2);
-        int i2 = Integer.valueOf("1101",2);
-        String s = Integer.toBinaryString((i1&i2));//
-        System.out.println(s); //1001*/
-        /*A = 0011 1100
-        B = 0000 1101
-        -----------------
-        A&B = 0000 1100
-        ~A= 1100 0011
-        A | B = 0011 1101
-        A ^ B = 0011 0001*/
-/*
-        System.out.println( Planet.MERCURY.equals(Planet.MERCURY) );//true
-        System.out.println( Planet.MERCURY==Planet.MERCURY );//true
-        System.out.println( Planet.MERCURY==Planet.VENUS );//false*
-*/
-/* //ASCII Table
-        System.out.println("A:"+(int)'A'); // or +(+'A') 65
-        System.out.println("Z:"+(+'Z'));//90
-        System.out.println("a:"+(+'a'));//97
-        System.out.println("z:"+(+'z'));//122
-        System.out.println("char(95):"+(char)95);//122
-        System.out.println('Z'-'A');
-        System.out.println('a'-'Z');
-        System.out.println('z'-'A');
-*/
+
+        System.out.println("hello"+ System.getProperty("line.separator")+"hello"); //get line separator
+        System.out.println("\\\n\\"); // "\\" = log \
 
     }
-        /*enum Planet {
-            MERCURY, VENUS, EARTH
-        }*/
-
 }
