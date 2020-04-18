4a6285fba9c03cc88e15b0614df6fc14faf41aa8
Youyan WU <wuyouyan1@gmail.com>
2020-04-18 18:30:32 +0200

    [Java IO stream] file Reader

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,35 @@
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.FileReader;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*TEST of Java！！！*/
+        FileReader example = new FileReader("D:\\wyy\\IdeaProjects\\Test_Java\\src\\hello.txt");
+        StreamPrinter.print(example);
 
-        ArrayList l = new ArrayList(2);
-        l.add(1);
-        l.add(1);
-        l.add(1);
-        System.out.println(l.size()); // 3
+    }
+}
+
+class StreamPrinter {
+    // TODO: strengthen the `print()`
+    // knowledge JAVA IO stream Reader
+    static void print(Reader reader) throws IOException {
+        int code = 0;
+        while ((code = reader.read()) != -1){
+            System.out.print((char) code);
+            code = reader.read();
+        }
+        /* or
+        while(true)
+        {
+            int code = reader.read();
+            if(code==-1)
+                break;
+             System.out.print((char)code);  //reader a char of the file
+        }
+        */
+        reader.close();
 
     }
 }
