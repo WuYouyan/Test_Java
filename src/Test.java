e377f5c5a463809467885937b40583094003c990
Youyan WU <wuyouyan1@gmail.com>
2020-04-22 15:01:13 +0200

    [Java Collections] HashMap.put() && pass by reference

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         /*TEST of Java！！！*/
-        String a = "A";
-        System.out.println(a.toLowerCase()); // return "a"
-        System.out.println(a); // A, the value of a is not changed
+        HashMap a = new HashMap<>();
+        Object o1 = new Object();
+        Object o2 = o1;
+        a.put(o1,1);
+        a.put(o2,2);
+        System.out.println(a.get(o1)); // 2
 
     }
 }
