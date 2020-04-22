eedc10b20c303ecc6b973544cee9ed17e92fac26
Youyan WU <wuyouyan1@gmail.com>
2020-04-22 13:57:40 +0200

    [Java String] toLowerCase() not change the value but return the changed value

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,11 @@
 import java.io.IOException;
-import java.math.BigInteger;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         /*TEST of Java！！！*/
-        BigInteger bi = new BigInteger("1");
-        bi.add(new BigInteger("1")); //return 2 the sum
-        System.out.println(bi); // 1 bi's value is not changed
+        String a = "A";
+        System.out.println(a.toLowerCase()); // return "a"
+        System.out.println(a); // A, the value of a is not changed
 
     }
 }
